a478650585bdb37ea27563971bc97d959e787f1a
Peter Okelmann <okelmann@in.tum.de>
2019-11-05 14:27:11 +0100

    add ringbuffer implementation from ranger-server

---
+++ src/plugins/benchplug/ringbuf.c
@@ -0,0 +1,87 @@
+/*
+ * TODO license smth
+ */
+#include <fcntl.h>
+#include <benchplug/ringbuf.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+// TODO use rbuf->sizeOfBuffer after creation during runtime
+
+sample_ringbuffer_t* sample_ringbuf_init() 
+{
+	sample_ringbuffer_t *rbuf = (sample_ringbuffer_t *) malloc(sizeof(sample_ringbuffer_t));
+	if (rbuf == NULL)
+		return NULL;
+	memset(rbuf, 0, sizeof(sample_ringbuffer_t));
+	rbuf->sizeOfBuffer = SAMPLE_RINGBUF_SIZE;
+	sample_ringbuf_reset(rbuf);
+	return rbuf;
+}
+
+void sample_ringbuf_destroy(sample_ringbuffer_t *rbuf)
+{
+	free(rbuf);
+}
+
+void sample_ringbuf_reset(sample_ringbuffer_t *rbuf)
+{
+	rbuf->headIndex = 0;
+	rbuf->tailIndex = 0;
+}
+
+// push onto top
+void sample_ringbuf_push(sample_ringbuffer_t *rbuf, uint32_t data)
+{
+	rbuf->n_rx_packets[rbuf->headIndex] = data;
+
+	// update headIndex
+	// this acts as (idx+1)%size because size is a power of two
+	rbuf->headIndex += 1;
+	rbuf->headIndex &= SAMPLE_RINGBUF_MAP;
+	if (rbuf->headIndex == rbuf->tailIndex )
+	{
+		// if we overwrite a 
+		rbuf->tailIndex += 1;
+		rbuf->tailIndex &= SAMPLE_RINGBUF_MAP;
+	}
+}
+
+// remove from bottom
+// return number of returned uwords (0 or 1)
+uint8_t sample_ringbuf_pop(sample_ringbuffer_t *rbuf, uint32_t *dst)
+{
+	if (rbuf->headIndex == rbuf->tailIndex) // rbuf is empty
+		return 0;
+
+	uint32_t n_rx_packets = rbuf->n_rx_packets[rbuf->tailIndex];
+	rbuf->tailIndex += 1;
+	rbuf->tailIndex &= SAMPLE_RINGBUF_MAP;
+
+	*dst = n_rx_packets;
+	return 1;
+}
+
+// requirement: sizeof(*n_rx_packets) == rbuf->sizeOfBuffer
+// uword *n_rx_packets is the destination
+// returns number of uwords copied
+uint32_t sample_ringbuf_extract_all(sample_ringbuffer_t *rbuf, uint32_t *n_rx_packets)
+{
+	// TODO not implemented
+	return 1;
+
+	uint32_t start_idx = rbuf->tailIndex;
+	if (rbuf->tailIndex <= rbuf->headIndex)
+	{
+		uint32_t n_copy = rbuf->headIndex - rbuf->tailIndex;
+		memcpy(n_rx_packets, rbuf->n_rx_packets , sizeof(uint32_t) * n_copy);
+	} else // tailIndex > headIndex
+	{
+		uint32_t n_copy = rbuf->headIndex - rbuf->tailIndex;
+		memcpy(n_rx_packets, rbuf->n_rx_packets, sizeof(uint32_t) * n_copy);
+		//memcpy()
+	}
+	//uint32_t stop_idx = min(start_idx, )
+}
---
+++ src/plugins/benchplug/ringbuf.h
@@ -0,0 +1,49 @@
+/*
+ * TODO license smth
+ */
+#ifndef __included__ringbuf_h__
+#define __included_ringbuf_h__
+
+// inspired by https://stackoverflow.com/a/1771607/4108673
+
+#include <stdint.h>
+
+/*
+12Mpps / 256 max. badge size =~ 47k badges per second
+=> 47k uwords/second
+=> 47k uwords/sec * 4 byte = 188kB/sec
+47k uwords/sec * 10 sec = 470k uwords
+188kB/sec * 10 sec = 1.88MB
+
+assuming a memcpy speed of 15000MB/s, a ringbuf memcpy takes:
+https://stackoverflow.com/questions/21038965/why-does-the-speed-of-memcpy-drop-dramatically-every-4kb
+1048576*4bytes = 4MB
+4MB / 15000MB/s = 0.267ms
+10Mpps * 0.267ms = 2.67K packets (0.0267% of all 
+	packets can't be processed immediately because auf the memcpy)
+=> no significant loss of throughput in high load scenarios
+=> probably enough packet buffer in low load scenarios
+*/
+// has to be a power of two and smaller than 2^32
+#define SAMPLE_RINGBUF_SIZE 1048576
+#define SAMPLE_RINGBUF_MAP 1048575 // SAMPLE_RINGBUF_SIZE - 1
+// capacity will be SAMPLE_RINGBUF_SIZE - 1
+
+typedef struct {
+	uint32_t headIndex; // next free field
+	uint32_t tailIndex; // oldest field or =headIndex if ring is empty
+	uint32_t sizeOfBuffer;
+	uint32_t n_rx_packets[SAMPLE_RINGBUF_SIZE];
+} sample_ringbuffer_t;
+
+sample_ringbuffer_t* sample_ringbuf_init();
+
+void sample_ringbuf_destroy(sample_ringbuffer_t *rbuf);
+
+void sample_ringbuf_reset(sample_ringbuffer_t *rbuf);
+
+void sample_ringbuf_push(sample_ringbuffer_t *rbuf, uint32_t data);
+
+uint8_t sample_ringbuf_pop(sample_ringbuffer_t *rbuf, uint32_t *dst);
+
+#endif /* __included_ringbuf_h__ */
